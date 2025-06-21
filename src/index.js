ebad8a83a0370d564460464d637a1bfe34e7998e
Srikanthkumardandaboina <srikanthdandaboina111@gmail.com>
2025-06-21 15:45:45 +0530

    added second name

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 function App(props){
   return(
-  <h1>Hello {props.name}</h1>
+  <h1>Hello {props.name} {props.secondname}</h1>
   );
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App name='Srikanth'/>
+    <App name='Srikanth' secondname='Dandaboina'/>
   </React.StrictMode>
 );
 
